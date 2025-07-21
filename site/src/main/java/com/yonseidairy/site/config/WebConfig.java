3684d1ab9b0b8722b1bb4f82c7a32a296eb08510
YouKnowKim <h3969@naver.com>
2025-07-21 11:18:36 +0900

    first

---
+++ site/src/main/java/com/yonseidairy/site/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.yonseidairy.site.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // 모든 경로 허용
+                .allowedOrigins("*") // 모든 Origin 허용
+                .allowedMethods("*") // 모든 HTTP 메서드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(false); // 쿠키 허용 여부 (필요 시 true로 설정)
+			}
+		};
+	}
+}
